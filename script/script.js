b1087a890fd08e53f46a28854e472a0ecbba18cb
Killer3048 <gamert11@mail.ru>
2023-02-06 16:14:50 +0700

    ssd

--- script/script.js
+++ script/script.js
@@ -2,22 +2,19 @@ const flyingObjects = document.querySelectorAll('.flying-object');
 const line = document.querySelector('.change-line');
 const lineWhite = document.querySelector('.change-line_type_white');
 const lineBlack = document.querySelector('.change-line_type_black');
+const cursor = document.querySelector(".cursor");
+const buttons = document.querySelectorAll("button");
 
-
-
-flyingObjects.forEach((object, index) => {
+flyingObjects.forEach((object) => {
     const randomDelay = Math.random() * 10;
-    const randomDuration = 10;
-    const randomStartX = Math.random() * 100;
     object.style.animationDelay = `${randomDelay}s`;
-    object.style.animationDuration = `${randomDuration}s`;
-    object.style.left = `${randomStartX}%`;
-    object.style.top = `${100}%`;
+    object.style.animationDuration = "10s";
+    object.style.left = `${Math.random() * 100}%`;
+    object.style.top = `100%`;
 
-    object.addEventListener('animationend', (event) => {
-        const randomStartX = Math.random() * 100;
-        object.style.left = `${randomStartX}%`;
-        object.style.top = `${100}%`;
+    object.addEventListener('animationend', () => {
+        object.style.left = `${Math.random() * 100}%`;
+        object.style.top = `100%`;
     });
 });
 
@@ -25,24 +22,17 @@ function changeColorLine() {
     lineWhite.classList.toggle('change-line_type_white-value');
     lineBlack.classList.toggle('change-line_type_black-value');
 }
-
 line.addEventListener('click', changeColorLine);
 
-
-
-const cursor = document.querySelector(".cursor");
-const buttons = document.querySelectorAll("button");
-
-
-document.addEventListener("mousemove", e => {
-    cursor.style.opacity = '1';
-    cursor.style.top = e.pageY + "px";
-    cursor.style.left = e.pageX + "px";
+document.addEventListener("mousemove", (e) => {
+    cursor.style.top = `${e.pageY}px`;
+    cursor.style.left = `${e.pageX}px`;
 });
 
-document.addEventListener("mouseenter", e => {
+document.addEventListener("mouseenter", (e) => {
+    cursor.style.opacity = '1';
     if (e.target.tagName === "BUTTON" || e.target.tagName === "INPUT") {
-        cursor.style.backgroundColor = "#00ff00";
+        cursor.style.backgroundColor = "red";
         cursor.style.width = "30px";
         cursor.style.height = "30px";
     } else {
@@ -52,7 +42,11 @@ document.addEventListener("mouseenter", e => {
     }
 });
 
-buttons.forEach(button => {
+document.addEventListener("mouseleave", () => {
+    cursor.style.opacity = '0';
+});
+
+buttons.forEach((button) => {
     button.addEventListener("mouseenter", () => {
         cursor.style.backgroundColor = "red";
     });
