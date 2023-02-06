3f557eca98a955f658c952b19337d750e07763fc
VEM <96580906+Killer3048@users.noreply.github.com>
2023-02-06 15:48:35 +0700

    Update script.js

--- script/script.js
+++ script/script.js
@@ -40,7 +40,7 @@ document.addEventListener("mousemove", e => {
     cursor.style.left = e.pageX + "px";
 });
 
-documetn.addEventListener("mouseleave", e = {
+document.addEventListener("mouseleave", e = {
     cursor.style.opacity = '1';
 });
 
