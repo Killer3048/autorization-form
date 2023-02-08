d0ebec3c29fcad7260ede5803e358ad5ecdb1e41
Killer3048 <gamert11@mail.ru>
2023-02-08 09:21:42 +0700

    ssd

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 const flyingObjects = document.querySelectorAll('.flying-object');
 const line = document.querySelector('.change-line');
 const lineWhite = document.querySelector('.change-line_type_white');
@@ -37,40 +36,33 @@ function changeColorLine() {
 }
 line.addEventListener('click', changeColorLine);
 
-if (isSafari) {
-    cursor.style.cursor = 'default';
-    cursor.style.pointerEvents = 'none';
-    body.style.cursor = 'pointer';
-} else {
+document.addEventListener("mousemove", (e) => {
+    cursor.style.top = `${e.pageY}px`;
+    cursor.style.left = `${e.pageX}px`;
+});
 
-    document.addEventListener("mousemove", (e) => {
-        cursor.style.top = `${e.pageY}px`;
-        cursor.style.left = `${e.pageX}px`;
+document.addEventListener("mouseenter", (e) => {
+    cursor.style.opacity = '1';
+    if (e.target.tagName === "BUTTON" || e.target.tagName === "INPUT") {
+        cursor.style.width = "30px";
+        cursor.style.height = "30px";
+    } else if (e.target instanceof Element) {
+        cursor.style.backgroundColor = getComputedStyle(e.target).backgroundColor;
+        cursor.style.width = "20px";
+        cursor.style.height = "20px";
+    }
+});
+
+
+document.addEventListener("mouseleave", () => {
+    cursor.style.opacity = '0';
+});
+
+buttons.forEach((button) => {
+    button.addEventListener("mouseenter", () => {
+        cursor.style.backgroundColor = "red";
     });
-
-    document.addEventListener("mouseenter", (e) => {
-        cursor.style.opacity = '1';
-        if (e.target.tagName === "BUTTON" || e.target.tagName === "INPUT") {
-            cursor.style.width = "30px";
-            cursor.style.height = "30px";
-        } else if (e.target instanceof Element) {
-            cursor.style.backgroundColor = getComputedStyle(e.target).backgroundColor;
-            cursor.style.width = "20px";
-            cursor.style.height = "20px";
-        }
+    button.addEventListener("mouseleave", () => {
+        cursor.style.backgroundColor = "#00ff00";
     });
-
-
-    document.addEventListener("mouseleave", () => {
-        cursor.style.opacity = '0';
-    });
-
-    buttons.forEach((button) => {
-        button.addEventListener("mouseenter", () => {
-            cursor.style.backgroundColor = "red";
-        });
-        button.addEventListener("mouseleave", () => {
-            cursor.style.backgroundColor = "#00ff00";
-        });
-    });
-};
+});
